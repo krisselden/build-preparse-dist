7b7b00d2595a1ee0e28d6a5079695b10c1b3c00a
Kris Selden <kris.selden@gmail.com>
2016-06-08 19:58:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ember Preparse Dist Builder
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+Install a version of Ember via bower then run build.
+
+```sh
+bower install ember#1.6.0
+npm run build
+```
---
+++ ember-cli-build.js
@@ -0,0 +1,8 @@
+var preparse = require('broccoli-ember-preparse');
+var Funnel = require('broccoli-funnel');
+
+module.exports = function() {
+  return preparse(new Funnel('bower_components/ember', {
+    exclude: ['.bower.json']
+  }), new Funnel('loader'));
+}
