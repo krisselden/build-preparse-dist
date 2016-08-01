98cae304eff0c164591b326c1462bffc34922c02
Kris Selden <kris.selden@gmail.com>
2016-08-01 12:37:13 -0700

    use new glimmer flag

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,7 +2,13 @@ var preparse = require('broccoli-ember-preparse');
 var Funnel = require('broccoli-funnel');
 
 module.exports = function() {
-  return preparse(new Funnel('bower_components/ember', {
-    exclude: ['.bower.json']
-  }), new Funnel('loader'));
+  return preparse(
+    new Funnel('bower_components/ember', {
+      exclude: ['.bower.json']
+    }),
+    new Funnel('loader'),
+    {
+      glimmer: true
+    }
+  );
 }
