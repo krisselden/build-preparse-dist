da2bf9e28a193cb234ae462de3bfc074882bd0e4
Kris Selden <kris.selden@gmail.com>
2016-08-04 13:53:27 -0700

    Configure eager load based on bower.json version.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,20 +1,87 @@
 var preparse = require('broccoli-ember-preparse');
 var Funnel = require('broccoli-funnel');
 var Filter = require('broccoli-persistent-filter');
+var fs = require('fs');
+var SemVer = require('semver');
+
+var bowerConfig = JSON.parse(fs.readFileSync('bower_components/ember/bower.json', 'utf8'));
 
 
+var version = new SemVer(bowerConfig.version);
+console.log('building', version);
 
-module.exports = function() {
-  return preparse(
-    new StripSourceMap(new Funnel('bower_components/ember', {
-      exclude: ['.bower.json']
-    })),
-    {
-      glimmer: true
+var options = {
+  eagerLoad: [
+    'htmlbars-runtime/hooks',
+    'ember/index',
+    'ember-htmlbars/system/render-view',
+    'ember-metal/streams/key-stream',
+    'ember-runtime/mixins/-proxy'
+  ]
+};
+
+if (version.major === 2) {
+  if (version.minor > 6) {
+    if (version.minor === 7) {
+      options.eagerLoad = [
+        'htmlbars-runtime/hooks',
+        'ember-htmlbars/renderer',
+        'ember-htmlbars/component',
+        'ember-htmlbars/helper',
+        'ember-htmlbars/components/checkbox',
+        'ember-htmlbars/components/text_field',
+        'ember-htmlbars/components/text_area',
+        'ember-htmlbars/components/link-to',
+        'ember-htmlbars/setup-registry',
+        'ember-htmlbars/streams/key-stream',
+        'ember-runtime/mixins/-proxy',
+        'ember/index'
+      ];
+    } else {
+      if (version.prerelease[0] === 'alpha') {
+        options.eagerLoad = [
+          'glimmer-reference/index',
+          'ember-glimmer/components/checkbox',
+          'ember-glimmer/components/link-to',
+          'ember-glimmer/components/text_area',
+          'ember-glimmer/components/text_field',
+          'ember-glimmer/renderer',
+          'ember-glimmer/make-bound-helper',
+          'ember-glimmer/setup-registry',
+          'ember-glimmer/views/outlet',
+          'ember-glimmer/templates/outlet',
+          'ember-glimmer/templates/component',
+          'ember-glimmer/dom',
+          'ember/index'
+        ];
+      } else {
+        options.eagerLoad = [
+          'ember-htmlbars/renderer',
+          'ember-htmlbars/component',
+          'ember-htmlbars/helper',
+          'ember-htmlbars/components/checkbox',
+          'ember-htmlbars/components/text_field',
+          'ember-htmlbars/components/text_area',
+          'ember-htmlbars/components/link-to',
+          'ember-htmlbars/utils/string',
+          'ember-htmlbars/make-bound-helper',
+          'ember-htmlbars/setup-registry',
+          'ember-htmlbars/index',
+          'ember-htmlbars/streams/key-stream',
+          'ember-runtime/mixins/-proxy',
+          'ember/index'
+        ];
+      }
     }
-  );
+  }
 }
 
+module.exports = function() {
+  var bowerEmber = new Funnel('bower_components/ember', {
+    exclude: ['.bower.json']
+  });
+  return preparse(new StripSourceMap(bowerEmber), options);
+}
 
 StripSourceMap.prototype = Object.create(Filter.prototype);
 
